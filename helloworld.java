e9a68d924b93a06f631a5c953c2447cba6e71279
Surya Tiwari <surya23tiwari@gmail.com>
2025-04-12 13:23:33 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,7 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello batch38 team with Praveen, Mustafa");
-      System.out.println("Hello Batch batch38 WITH FOR STUDENTS RUNNING in front of me and Suresh, Prashant in making CI automated vikas teach devops CI proecss");
+     System.out.println("Hello All, Hope you are doing well.");
+      System.out.println("Hello Everyone, "Success is not final, failure is not fatal: It is the courage to continue that counts."
+");
     }  
 } 
